12b3d6bf125f67a16453585f0b707be3e9ad7d58
veler86 <zasadzkimarcin@gmail.com>
2018-11-21 05:29:42 +0000

    check form validity

--- js/map.js
+++ js/map.js
@@ -33,6 +33,11 @@
   var PIN_HIGHT = 62 + 22;
   var HALF_PIN_WIDTH = 31 + 5;
 
+  var BUNGALO_MIN_PRICE = 0;
+  var FLAT_MIN_PRICE = 10;
+  var HOUSE_MIN_PRICE = 50;
+  var PALACE_MIN_PRICE = 100;
+
   var mapElement = document.querySelector('.map');
   var mapPinsElement = document.querySelector('.map__pins');
   var pinElementTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -41,6 +46,12 @@
   var adFormContainer = document.querySelector('.ad-form');
   var formAddressInput = adFormContainer.querySelector('#address');
   var mapPinsMainElement = mapPinsElement.querySelector('.map__pin--main');
+  var priceElement = adFormContainer.querySelector('#price');
+  var propertyTypeElement = adFormContainer.querySelector('#type');
+  var checkinElement = adFormContainer.querySelector('#timein');
+  var checkoutElement = adFormContainer.querySelector('#timeout');
+  var capacityElement = adFormContainer.querySelector('#capacity');
+  var roomsElement = adFormContainer.querySelector('#room_number');
 
   function getRandomNumberInRange(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -165,6 +176,23 @@
     setFormInputsState(false);
   }
 
+  function checkGuestsField() {
+    var guests = +capacityElement.value;
+    var numberOfRooms = +roomsElement.value;
+
+    if (numberOfRooms === 1 && guests !== 1) {
+      capacityElement.setCustomValidity('Too many guests for one room');
+    } else if (numberOfRooms === 2 && guests > 2) {
+      capacityElement.setCustomValidity('Too many guests for two rooms');
+    } else if (numberOfRooms === 3 && guests > 3) {
+      capacityElement.setCustomValidity('Too many guests for three rooms');
+    } else if (numberOfRooms === 100 && guests > 0) {
+      capacityElement.setCustomValidity('Not for guests');
+    } else {
+      capacityElement.setCustomValidity('');
+    }
+  }
+
   function mouseUpHandler(event) {
     var currentTarget = event.currentTarget;
     var generatedPinElements = generatePins();
@@ -176,6 +204,7 @@
     updateAddress(pinLocation);
     mapPinsElement.appendChild(generatedPinElements);
     currentTarget.removeEventListener('mouseup', mouseUpHandler);
+    checkGuestsField();
   }
 
 
@@ -215,6 +244,41 @@
     event.currentTarget.removeEventListener('click', closeCardHandler);
   }
 
+  function typeFieldHandler(event) {
+    switch (event.currentTarget.value) {
+      case ('bungalo'):
+        priceElement.min = BUNGALO_MIN_PRICE;
+        priceElement.placeholder = BUNGALO_MIN_PRICE;
+        break;
+      case ('flat'):
+        priceElement.min = FLAT_MIN_PRICE;
+        priceElement.placeholder = FLAT_MIN_PRICE;
+        break;
+      case ('house'):
+        priceElement.min = HOUSE_MIN_PRICE;
+        priceElement.placeholder = HOUSE_MIN_PRICE;
+        break;
+      case ('palace'):
+        priceElement.min = PALACE_MIN_PRICE;
+        priceElement.placeholder = PALACE_MIN_PRICE;
+        break;
+    }
+  }
+
+  function checkinHandler(event) {
+    var checkinTime = event.currentTarget.value;
+    checkoutElement.value = checkinTime;
+  }
+
+  function checkoutHandler(event) {
+    var checkoutTime = event.currentTarget.value;
+    checkinElement.value = checkoutTime;
+  }
+
+  function guestsHandler() {
+    checkGuestsField();
+  }
+
   disableFormInputs();
 
   mapPinsMainElement.addEventListener('mouseup', mouseUpHandler);
@@ -226,4 +290,9 @@
   }
 
   mapElement.addEventListener('mouseup', pinsHandler);
+  propertyTypeElement.addEventListener('change', typeFieldHandler);
+  checkinElement.addEventListener('change', checkinHandler);
+  checkoutElement.addEventListener('change', checkoutHandler);
+  roomsElement.addEventListener('change', guestsHandler);
+  capacityElement.addEventListener('change', guestsHandler);
 }());
